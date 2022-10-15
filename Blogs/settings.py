e7774c2374bda6eb28c0e299148e59511b029966
Seba-Galli <sebastianegalli@gmail.com>
2022-10-15 14:55:56 -0300

    fin

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0e&^cr!m-v&4=ps4##aog$991b2j5lc2rule7=@^phcf0n5pi7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sleepy-garden-34452.herokuapp.com/AppBlogs/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
